0d7c9edd12fc420327ff56304660e909399f147a
Dane Kilpatrick <danekilpatrick@Danes-MacBook-Air.local>
2019-01-31 21:04:45 -0600

    Changed the name of the challenge.js file to students.js | Created students.js file

--- objects.js
+++ objects.js
@@ -1,32 +1,32 @@
-var person = {
-    name: 'Sarah',
-    country: 'United States',
-    age: 35,
-    treehouseStudent: true,
-    skills: ['Javascript', 'HTML', 'CSS']
-};
-
-person.gender = 'female';
-
-console.log(person);
-
-function print(message) {
-  var div = document.getElementById('output');
-  div.innerHTML = message;
-}
+// var person = {
+//     name: 'Sarah',
+//     country: 'United States',
+//     age: 35,
+//     treehouseStudent: true,
+//     skills: ['Javascript', 'HTML', 'CSS']
+// };
 //
-// var message = "<p>Hell my name is " + person.name + "</p>";
-// message += "<p>I live in the " + person.country + ".</p>";
-// person.name = 'Rainbow Dash';
-// message += '<p>But, I wish my name was ' + person.name + '.</p>';
-// person.age += 1;
-// message += '<p>My age is now ' + person.age + '</p>';
-// // message += '<p>I have ' + person.skills.length + ' skills: ' + person.skills.join(', ') + '</p>'; //the next two lines are the same as this one.
-// message += '<p>I have ' + person.skills.length + ' skills: ';
-// message += person.skills.join(', ') + '</p>';
-// print(message);
+// person.gender = 'female';
 //
-
-for (prop in person) {
-  console.log(prop,': ', person[prop]);
-}
+// console.log(person);
+//
+// function print(message) {
+//   var div = document.getElementById('output');
+//   div.innerHTML = message;
+// }
+// //
+// // var message = "<p>Hell my name is " + person.name + "</p>";
+// // message += "<p>I live in the " + person.country + ".</p>";
+// // person.name = 'Rainbow Dash';
+// // message += '<p>But, I wish my name was ' + person.name + '.</p>';
+// // person.age += 1;
+// // message += '<p>My age is now ' + person.age + '</p>';
+// // // message += '<p>I have ' + person.skills.length + ' skills: ' + person.skills.join(', ') + '</p>'; //the next two lines are the same as this one.
+// // message += '<p>I have ' + person.skills.length + ' skills: ';
+// // message += person.skills.join(', ') + '</p>';
+// // print(message);
+// //
+//
+// for (prop in person) {
+//   console.log(prop,': ', person[prop]);
+// }
---
+++ students.js
@@ -0,0 +1,27 @@
+var students = [
+  {name: 'Dane',
+   stack: 'Front-End Development',
+   achievements: '2345',
+   points: '40'
+  },
+  {name: 'DJ',
+   stack: 'ios Development with Swift',
+   achievements: '934',
+   points: '3500'
+  },
+  {name: 'Britton',
+   stack: 'PHP Development',
+   achievements: '834',
+   points: '500'
+  },
+  {name: 'Chloe',
+   stack: 'Learn Wordpress',
+   achievements: '324',
+   points: '500'
+  },
+  {name: 'Tyler',
+   stack: 'Back-End Development',
+   achievements: '734',
+   points: '500'
+  }
+];
