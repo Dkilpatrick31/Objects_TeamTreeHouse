0b124a24584a8b6308e6dd308f7d7e0602a7b66e
Dane Kilpatrick <danekilpatrick@Danes-MacBook-Air.local>
2019-01-31 19:59:27 -0600

    Created a for-in loop that looped through the properties of the person object

--- objects.js
+++ objects.js
@@ -14,14 +14,19 @@ function print(message) {
   var div = document.getElementById('output');
   div.innerHTML = message;
 }
+//
+// var message = "<p>Hell my name is " + person.name + "</p>";
+// message += "<p>I live in the " + person.country + ".</p>";
+// person.name = 'Rainbow Dash';
+// message += '<p>But, I wish my name was ' + person.name + '.</p>';
+// person.age += 1;
+// message += '<p>My age is now ' + person.age + '</p>';
+// // message += '<p>I have ' + person.skills.length + ' skills: ' + person.skills.join(', ') + '</p>'; //the next two lines are the same as this one.
+// message += '<p>I have ' + person.skills.length + ' skills: ';
+// message += person.skills.join(', ') + '</p>';
+// print(message);
+//
 
-var message = "<p>Hell my name is " + person.name + "</p>";
-message += "<p>I live in the " + person.country + ".</p>";
-person.name = 'Rainbow Dash';
-message += '<p>But, I wish my name was ' + person.name + '.</p>';
-person.age += 1;
-message += '<p>My age is now ' + person.age + '</p>';
-// message += '<p>I have ' + person.skills.length + ' skills: ' + person.skills.join(', ') + '</p>'; //the next two lines are the same as this one.
-message += '<p>I have ' + person.skills.length + ' skills: ';
-message += person.skills.join(', ') + '</p>';
-print(message);
+for (prop in person) {
+  console.log(prop,': ', person[prop]);
+}
